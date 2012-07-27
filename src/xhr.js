9c955e2b560ca9fbc10ce4c51a7789ea840f0bcc
Chase Wilson <chase@agroism.com>
2012-07-26 23:49:10 -0700

    Added `request`, which depends on `xhr` Removed all the request code from `xhr`

--- src/xhr.js
+++ src/xhr.js
@@ -16,59 +16,6 @@ var XHR = (function(){
   }())
 }())
 
-function ie8Request (method, url, data, callback) {
-  var xdr = new XDomainRequest()
-  xdr.onload = function() {
-    var response
-    try {
-      response = JSON.parse(xdr.responseText)
-    } catch (e) {
-      callback && callback(xdr)
-    }
-    
-    if (response) {
-      callback && callback(null, response, xdr)
-    }
-  }
-
-  xdr.onerror = xdr.ontimeout = function() {
-    callback && callback(xdr)
-  }
-
-  xdr.onprogress = function() {}
-  xdr.open(method, url)
-  xdr.send(data)
-}
-
-function xhrRequest (method, url, data, callback) {
-  if (typeof(XDomainRequest) !== "undefined") {
-    return ie8Request(method, url, data, callback)
-  }
-
-  var xhr = new XHR()
-  xhr.onreadystatechange = function() {
-    var response
-    if (xhr.readyState === 4) {
-      if (xhr.status >= 200 && xhr.status < 300) {
-        try {
-          response = JSON.parse(xhr.responseText)
-        } catch (e) {
-          callback && callback(xhr)
-        }
-        if (response) {
-          callback && callback(null, response, xhr)
-        }
-      } else {
-        callback && callback(xhr)
-      }
-    }
-  }
-
-  xhr.open(String(method).toUpperCase(), url, true)
-  xhr.setRequestHeader("Content-Type", "text/plain")  // avoid pre-flight.
-  xhr.send(data)
-}
-
-module.exports = xhrRequest
+module.exports = XHR
 
 // @sourceURL = yaul/xhr.js
